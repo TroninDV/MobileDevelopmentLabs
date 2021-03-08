52deb5b34efbde040abfc89b469ab9490284d276
TroninDV <tronindmitr@gmail.com>
2021-03-08 23:25:25 +0300

    Update first lab (move click listener to lambda)

--- Lab1/app/src/main/java/com/tronindmitr/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/tronindmitr/lab1/MainActivity.kt
@@ -3,24 +3,23 @@ package com.tronindmitr.lab1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var currentCounter = 0
-    lateinit var text: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        text = findViewById(R.id.counter_text)
+        val text = findViewById<TextView>(R.id.counter_text)
         text.text = "0"
+        var currentCounter = 0
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            currentCounter++
+            text.text = currentCounter.toString()
+        }
     }
-
-    fun incrementOnClick(view: View) {
-        currentCounter++
-        text.text = currentCounter.toString()
-    }
-
-
 }
